af1ea616dd0b555fb49acd512bd519900fb06a83
Sayopontes <yonnapontes@hotmail.com>
2024-10-04 18:46:24 -0300

    atulização do exercicio 15

--- Aula 12 Ex/ex015/scrip.js
+++ Aula 12 Ex/ex015/scrip.js
@@ -2,56 +2,59 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano') // f de formulário
-    var res = document.getElementById('res')
+    var res = document.querySelector('div#res')
 
-    if (fano.ariaValueMax.length == 0 || Number(fano.value) > ano){
+    if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('[ERRO] verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gênero = ''
+        var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         
         if (fsex[0].checked) {
-            gênero = 'Homem';
+            genero = 'Homem';
             if (idade >= 0 && idade < 10) {
                 // criança
                 img.setAttribute('src', 'criançamasculino.jpg')
 
             } else if (idade < 21) {
                 // jovem
-                img.setAttribute('src', 'jovemmasculino')
+                img.setAttribute('src', 'jovemmasculino.jpg')
 
             } else if (idade < 50) {
                 // adulto
-                img.setAttribute('src', 'adultomasculino')
+                img.setAttribute('src', 'adultomasculino.jpg')
                 
             } else {
                 // idoso
-                img.setAttribute('src', 'adultomasculino')
+                img.setAttribute('src', 'adultomasculino.jpg')
             }
         } else if (fsex[1].checked) {
-            gênero = 'Mulher';
+            genero = 'Mulher';
             if (idade >= 0 && idade < 10) {
                 // criança
                 img.setAttribute('src', 'criançfeminino.jpg')
 
             } else if (idade < 21) {
                 // jovem
-                img.setAttribute('src', 'jovemmasculino')
+                img.setAttribute('src', 'jovemmasculino.jpg')
 
             } else if (idade < 50) {
                 // adulto
-                img.setAttribute('src', 'adultomasculino')
+                img.setAttribute('src', 'adultomasculino.jpg')
 
             } else {
                 // idoso
-                img.setAttribute('src', 'adultomasculino')
+                img.setAttribute('src', 'adultomasculino.jpg')
 
             }
         }
+        res.style.textAlign = 'center'
+        res.InnerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     
-}
+    }
 }
 
